0d6771429ec67090235e250b471f85827a304ec5
Angelo Veltens <angelo.veltens@codecentric.de>
2022-01-26 20:42:41 +0100

    [get-current-weather] respond with requested locality instead of result from api

--- skills/weather/src/get-current-weather/handler.spec.ts
+++ skills/weather/src/get-current-weather/handler.spec.ts
@@ -1,56 +1,54 @@
-import {RhasspyEvent, RhasspySession} from "../../../../core/src";
-import {WeatherConditions, WeatherService} from "../service";
-import {GetCurrentWeather} from "./handler";
+import { RhasspyEvent, RhasspySession } from "../../../../core/src";
+import { WeatherConditions, WeatherService } from "../service";
+import { GetCurrentWeather } from "./handler";
 
-describe('get current weather', () => {
+describe("get current weather", () => {
+  let session: RhasspySession;
 
-    let session: RhasspySession;
+  beforeEach(() => {
+    session = {
+      say: jest.fn(),
+    };
+  });
 
-    beforeEach(() => {
-        session = {
-            say: jest.fn(),
-        };
+  it("returns the current weather conditions of the asked location", async () => {
+    const conditions: WeatherConditions = {
+      description: "Nebel",
+      temperatureInCelsius: "18",
+      locality: "Hamburg",
+    };
+    const weatherService: WeatherService = {
+      getCurrentConditions: jest.fn().mockResolvedValue(conditions),
+    };
+    await new GetCurrentWeather(weatherService).handle(
+      {
+        slots: [
+          {
+            value: { value: "Hamburg" },
+            slotName: "locality",
+          },
+        ],
+      } as unknown as RhasspyEvent,
+      session
+    );
+    expect(weatherService.getCurrentConditions).toHaveBeenCalledWith("Hamburg");
+    expect(session.say).toHaveBeenCalledWith(
+      "In Hamburg sind es aktuell 18 Grad. Es ist Nebel."
+    );
+  });
+
+  describe("fails", () => {
+    it("when no locality slot is given", async () => {
+      const event = {
+        slots: [],
+      } as unknown as RhasspyEvent;
+      const session = {
+        say: jest.fn(),
+      };
+      await new GetCurrentWeather({} as WeatherService).handle(event, session);
+      expect(session.say).toHaveBeenCalledWith(
+        "Ich habe nicht verstanden, um welchen Ort es geht."
+      );
     });
-
-    it('returns the current weather conditions of the asked location', async () => {
-        const conditions: WeatherConditions = {
-            description: 'Nebel',
-            temperatureInCelsius: "18",
-            locality: 'Hamburg'
-        }
-        const weatherService: WeatherService = {
-            getCurrentConditions: jest.fn().mockResolvedValue(conditions)
-        }
-        await new GetCurrentWeather(weatherService).handle({
-                slots: [
-                    {
-                        value: { value: "hamburg" },
-                        slotName: "locality",
-                    },
-                ],
-            } as unknown as RhasspyEvent,
-            session);
-        expect(weatherService.getCurrentConditions).toHaveBeenCalledWith('hamburg');
-        expect(session.say).toHaveBeenCalledWith('In Hamburg sind es aktuell 18 Grad. Es ist Nebel.')
-    });
-
-    describe("fails", () => {
-        it("when no locality slot is given", async () => {
-            const event = {
-                slots:  [],
-            } as unknown as RhasspyEvent;
-            const session = {
-                say: jest.fn(),
-            };
-            await new GetCurrentWeather(
-                {} as WeatherService
-            ).handle(
-                event,
-                session
-            );
-            expect(session.say).toHaveBeenCalledWith(
-                "Ich habe nicht verstanden, um welchen Ort es geht."
-            );
-        });
-    });
+  });
 });
