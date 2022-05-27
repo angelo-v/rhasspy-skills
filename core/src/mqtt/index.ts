6d30f8686c10c4e57a00387a272d3fcb1752d420
Angelo Veltens <angelo.veltens@online.de>
2022-05-27 18:21:13 +0200

    allow to authenticate against mqtt broker

--- core/src/mqtt/index.ts
+++ core/src/mqtt/index.ts
@@ -1,9 +1,18 @@
 import * as mqtt from "mqtt";
-import {MqttClient} from "mqtt";
-import {mqttSkill} from "./skill";
+import { MqttClient } from "mqtt";
+import { mqttSkill } from "./skill";
 
-const brokerUrl = process.env.MQTT_BROKER_URL ?? "mqtt://localhost:12183"
+const brokerUrl = process.env.MQTT_BROKER_URL ?? "mqtt://localhost:12183";
+const username = process.env.MQTT_USERNAME;
+const password = process.env.MQTT_PASSWORD;
 
-const client: MqttClient = mqtt.connect(brokerUrl);
+const mqttOptions = username
+  ? {
+      username,
+      password,
+    }
+  : {};
+
+const client: MqttClient = mqtt.connect(brokerUrl, mqttOptions);
 
 export const skill = mqttSkill(client);
