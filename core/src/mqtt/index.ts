bf44350fa981d97a26bf5036cc4446d1a8086ecd
Angelo Veltens <angelo.veltens@online.de>
2022-05-27 20:52:52 +0200

    allow to assign mqtt client id and assign random by default

--- core/src/mqtt/index.ts
+++ core/src/mqtt/index.ts
@@ -5,13 +5,15 @@ import { mqttSkill } from "./skill";
 const brokerUrl = process.env.MQTT_BROKER_URL ?? "mqtt://localhost:12183";
 const username = process.env.MQTT_USERNAME;
 const password = process.env.MQTT_PASSWORD;
+const clientId = process.env.MQTT_CLIENT_ID ?? `rhasspy-skill-${Math.floor(Math.random()*1000000000)}`;
 
 const mqttOptions = username
   ? {
+      clientId,
       username,
       password,
     }
-  : {};
+  : { clientId };
 
 const client: MqttClient = mqtt.connect(brokerUrl, mqttOptions);
 
